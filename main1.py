8d9cab4c76d103740bbae1316ceb03dd01ec1f5e
tishenko-va <valeriaalistarhova778@gmail.com>
2024-07-04 11:33:33 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+# "1st program"
+print((9.0 ** 0.5) * 5)
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+# "4th program"
+print(13.42 // 1 == int((42.13 - int(42.13)) * 100)) and int((13.42 - int(13.42)) == 42.13 // 1)
+
+
